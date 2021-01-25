a87f9476f82680be93bb0e70f81a8322d6ea436b
Steven Machado <steve.machado64@gmail.com>
2021-01-25 17:48:08 +0000

    submit assignment 2-1

---
+++ assignment2-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int checking;
+  unsigned int miles;
+  long days;
+  checking = -20;
+  miles = 4276;
+  days = 189000;
+
+  cout << "We have made a long journey of " << miles << " miles.\n";
+  cout << "Our checking account balance is " << checking;
+  cout << "\nAbout " << days << " days ago Columbus stood on this spot.\n";
+  return 0;
+}
