025177a2a07c0520882ae43a167fb2777f676596
Steven Machado <steve.machado64@gmail.com>
2021-01-27 18:12:40 +0000

    added assignment 2-6 challenge 2

---
+++ assignment 2-6 c2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int variable1 = 28, variable2 = 32, variable3 = 37,variable4 = 24, variable5 = 33;
+
+  double total = variable1 + variable2 + variable3 + variable4 + variable5;
+  double average = total / 5;
+
+  cout << "Total: " << total << endl;
+  cout << "Average: " << average << endl;
+}
