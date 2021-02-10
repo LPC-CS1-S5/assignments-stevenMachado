6ffa40c201de67638ed5b10374f9f13a914d7589
Steven Machado <steve.machado64@gmail.com>
2021-02-10 05:10:23 +0000

    add assignment 3-2

---
+++ assign 3-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int numPeople,numMales,numFemales;
+  double percentM,percentF;
+  cout << "How many people total are in the class: ";
+  cin >> numPeople;
+  cout << "How many are males: ";
+  cin >> numMales;
+  cout << "How many are females: ";
+  cin >> numFemales;
+
+  percentF = (double(numFemales) / numPeople);
+  percentM = (double(numMales) / numPeople) * 100;
+
+ cout << "The percentage of females is " << percentF << "%.\n";
+ cout << "The percentage of males is " << percentM << "%.\n";
+}
